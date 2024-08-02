4689ebb4175c706bfda97ef80a20422db8598e41
sarwar-asik <sarwarasik@gmail.com>
2024-08-03 03:02:39 +0600

    video: implemented video @stream

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,6 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import app from './app';
 import { chatHandler } from './app/socket/chat/chatHandle';
-import { roomHandler } from './app/socket/room';
 // const app: Application = express();
 
 const httpServer = createServer(app);
@@ -21,7 +20,42 @@ io.on('connection', socket => {
   socket?.id = socket?.handshake?.query?.id;
   connectedClients[socket?.id] = socket;
 
-  roomHandler(socket);
+  // roomHandler(socket);
+  // console.log(socket.id, 'socket.id');
+  socket.on('offer', offerData => {
+    console.log('>>offer', offerData);
+    if (connectedClients[offerData.targetId]) {
+      // console.log('Sending incoming-call to:', offerData.targetId);
+      connectedClients[offerData.targetId].emit('incoming-call', {
+        offer: offerData.offer,
+        from: socket.id,
+        senderName: offerData.senderName,
+      });
+    }
+  });
+
+  socket.on('answer', answerData => {
+    console.log(answerData);
+    // const targetId = socket.handshake.query.targetId;
+    if (connectedClients[answerData.targetId]) {
+      connectedClients[answerData.targetId].emit('answer', answerData.answer);
+    }
+  });
+
+  socket.on('ice-candidate', candidate => {
+    const targetId = socket.handshake.query.targetId as string
+    if (connectedClients[targetId]) {
+      connectedClients[targetId].emit('ice-candidate', candidate);
+    }
+  });
+
+  socket.on('end-call', receiverId => {
+    // const targetId = socket.handshake.query.targetId;
+    if (connectedClients[receiverId]) {
+      connectedClients[receiverId].emit('end-call');
+    }
+  });
+
   chatHandler(socket, connectedClients);
 
   socket.on('disconnect', () => {
